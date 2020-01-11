8f98536ede65d58f49ab76532b7262670b24c2b8
shoval6 <shovalhm@gmail.com>
2020-01-11 18:26:26 +0200

    syntax fixing

--- server.js
+++ server.js
@@ -24,7 +24,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(cors());
 
-app.get('/',(req, res) => {res.send(`it's working`);
+app.get('/',(req, res) => {res.send(`it's working`)});
 app.post('/signin',(req,res) => {signin.handleSignin(req, res, db, bcrypt)});
 app.post('/register',(req,res) => { register.handleRegister(req, res, db, bcrypt)});
 app.get('/profile/:id',(req,res) => { profile.handleProfileGet(req, res, db)});
@@ -34,4 +34,4 @@ app.post('/imageurl',(req,res) => { image.handleApiCall(req, res)});
 
 app.listen(process.env.PORT || 3000,() => { 
   console.log(`app is running on port ${process.env.PORT}`);
-})
+});
